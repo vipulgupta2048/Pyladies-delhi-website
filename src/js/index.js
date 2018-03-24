cb14500d241d8bc036626bca8f3664a32b4b390c
Ayush Agarwal <thisisayushaa@gmail.com>
2018-03-24 23:05:27 +0530

    Remove Unnecessary Files

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-
-setTimeout(function opening(){
-  document.getElementsByClassName("leftbanner")[0].style.width="0px";
-   document.getElementsByClassName("rightbanner")[0].style.width="0px";
-},1500);
